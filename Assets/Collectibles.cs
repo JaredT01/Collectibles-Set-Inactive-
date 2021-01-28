603c21c7d0dbb3f26a185a0932f880c6ab98bf9d
RoyalGuilds <Jared.trenholm12@gmail.com>
2021-01-28 14:17:21 -0400

    Collectibles Created
    Collectibles have been created that just turn inactive once claimed.

---
+++ Assets/Collectibles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectibles : MonoBehaviour
+{
+        public Text points;
+    private int score = 0;
+    private void Update()
+    {
+        points.text = "Count: " + score.ToString();
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag == "Collectible")
+        {
+            other.gameObject.SetActive(false);
+            score = score + 1;
+        }
+    }
+}
